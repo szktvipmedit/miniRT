b412f4fd436fa4de0669687e75ac9fde1d54cac1
Kouta Suzuki <kousuzuk@c1r14s3.42tokyo.jp>
2023-12-22 14:13:02 +0900

    overflowを桁判定にした

--- srcs/utils2.c
+++ srcs/utils2.c
@@ -14,40 +14,35 @@ static int sign_flag(char *str, size_t *i)
 	return minus_flag;
 }
 
-static int	ft_isoverflow(long long nbr, unsigned int digit, int sign)
-{
-	if (sign < 0)
-		return ((DBL_MIN + digit) / 10 > (-1) * nbr);
-	if (sign > 0)
-		return ((DBL_MAX - digit) / 10 < nbr);
-	return (0);
-}
-
-static double integer_part(char *str, size_t *i, int sign)
+static double integer_part(char *str, size_t *i)
 {
 	double integer;
+	size_t int_len;
+
 	integer = 0;
+	int_len = 0;	
 	while(str[*i] && ft_isdigit(str[*i]))
     {
-		if (ft_isoverflow(integer, (str[*i] - '0'), sign))
+		if (int_len > 10)
 		{
 			ft_printf_stderr("The compatible range of input values is DBL_MIN to DBL_MAX\n");
 			exit(1);
 		}
         integer *= 10;
 		integer += str[*i] - '0';
+		int_len++;
         (*i)++;
     }
 	return integer;
 }
 
-static size_t decimal_part(char *str, double *decimal, size_t *i, int sign)
+static size_t decimal_part(char *str, double *decimal, size_t *i)
 {
 	size_t deci_len;
 	deci_len = 0;
 	while(str[*i] && ft_isdigit(str[*i]))
     {
-		if (ft_isoverflow(*decimal, (str[*i] - '0'), sign))
+		if (deci_len > 4)
 		{
 			ft_printf_stderr("The compatible range of input values is DBL_MIN to DBL_MAX\n");
 			exit(1);
@@ -70,13 +65,13 @@ double ft_atod(char *str)
     minus_flag = 1;
     
 	minus_flag = sign_flag(str, &i);
-	integer = integer_part(str, &i, minus_flag);
+	integer = integer_part(str, &i);
 	if(str[i] != '.')
 		return minus_flag * integer;
 	i++;
 	double decimal;
 	double deci_len;
 	decimal = 0;
-	deci_len = decimal_part(str, &decimal, &i, minus_flag);
+	deci_len = decimal_part(str, &decimal, &i);
     return minus_flag * (integer + (decimal / pow(10, deci_len)));
 }
